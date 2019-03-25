d44ca530bf74ccbc2949fb23df5910abfdd1e4de
asolonovich <andrey@solonovich.name>
2019-03-25 15:33:42 +0200

    EthQ unpadded value without leading zeroes

--- src/DataType/EthQ.php
+++ src/DataType/EthQ.php
@@ -269,7 +269,8 @@ class EthQ extends EthD
      */
     public function hexValUnpadded()
     {
-        return '0x' . $this->value->toHex($this->value->is_negative);
+        $clearZeroes = preg_replace('/$0+/', '', $this->value->toHex($this->value->is_negative));
+        return '0x' . $clearZeroes;
     }
 
     /**
